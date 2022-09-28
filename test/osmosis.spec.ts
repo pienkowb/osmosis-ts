ae92251871f338dd638d4d2955e2114cc3c50cfe
Bartosz Pieńkowski <pienkowb@gmail.com>
2022-09-28 19:26:12 +0200

    Format TypeScript files with Prettier

--- test/osmosis.spec.ts
+++ test/osmosis.spec.ts
@@ -1,33 +1,37 @@
 /// <reference types="@as-pect/assembly/types/as-pect" />
 
-import { encodeMsgSwapExactAmountOut, decodeMsgSwapExactAmountOut } from '../src/osmosis/gamm/v1beta1/tx';
-import { MsgSwapExactAmountOut, SwapAmountOutRoute } from '../src/osmosis/gamm/v1beta1/tx';
-import { Coin } from '../src/cosmos/base/v1beta1/coin';
+import {
+  MsgSwapExactAmountOut,
+  SwapAmountOutRoute,
+  encodeMsgSwapExactAmountOut,
+  decodeMsgSwapExactAmountOut,
+} from "../src/osmosis/gamm/v1beta1/tx";
+import { Coin } from "../src/cosmos/base/v1beta1/coin";
 
-describe('Osmosis', () => {
-  test('decodes a swap message', () => {
+describe("Osmosis", () => {
+  test("decodes a swap message", () => {
     const msg = new MsgSwapExactAmountOut(
-      'osmo1hjct6q7npsspsg3dgvzk3sdf89spmlpfqua7lv',
-      [new SwapAmountOutRoute(3, 'osmo')],
-      '1000',
-      new Coin('osmo', '100')
+      "osmo1hjct6q7npsspsg3dgvzk3sdf89spmlpfqua7lv",
+      [new SwapAmountOutRoute(3, "osmo")],
+      "1000",
+      new Coin("osmo", "100")
     );
 
     const encodedMsg = encodeMsgSwapExactAmountOut(msg);
     const decodedMsg = decodeMsgSwapExactAmountOut(encodedMsg);
 
-    expect(decodedMsg.sender).toBe('osmo1hjct6q7npsspsg3dgvzk3sdf89spmlpfqua7lv');
+    expect(decodedMsg.sender).toBe(msg.sender);
 
     const route = decodedMsg.routes[0] as SwapAmountOutRoute;
 
     expect(route.poolId).toBe(3);
-    expect(route.tokenInDenom).toBe('osmo');
+    expect(route.tokenInDenom).toBe("osmo");
 
-    expect(decodedMsg.tokenInMaxAmount).toBe('1000');
+    expect(decodedMsg.tokenInMaxAmount).toBe("1000");
 
     const tokenOut = decodedMsg.tokenOut as Coin;
 
-    expect(tokenOut.denom).toBe('osmo');
-    expect(tokenOut.amount).toBe('100');
+    expect(tokenOut.denom).toBe("osmo");
+    expect(tokenOut.amount).toBe("100");
   });
 });
