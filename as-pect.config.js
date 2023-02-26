fc04ed0199b057cdfa9ea162997affebf99ae8b5
Bartosz Pieńkowski <pienkowb@gmail.com>
2023-02-26 14:00:08 +0100

    Upgrade the dependencies

--- as-pect.config.js
+++ as-pect.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  include: ['test/**/*.ts'],
-  flags: {
-    "--runtime": ["stub"],
-  },
+export default {
+  entries: ["test/**/*.spec.ts"],
+
+  async instantiate(memory, createImports, instantiate, binary) {
+    let instance;
+    const myImports = {
+      env: { memory }
+    };
+    instance = instantiate(binary, createImports(myImports));
+    return instance;
+  }
 };
